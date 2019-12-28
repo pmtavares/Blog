b11e700d36c431b4c6f12180b996b16f55bb0482
pmtavares <pedrotavares41@gmail.com>
2019-12-28 17:17:38 +0000

    AddedContextProvider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import {BlogProvider} from './src/context/BlogContext'
 
 const navigator = createStackNavigator({
   Index: IndexScreen
@@ -16,5 +17,5 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />;
+  return <BlogProvider><App /></BlogProvider>;
 }
